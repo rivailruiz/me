77eb18c606f83ea5923c7c8b894ec03ff9ff1b9a
Ruiz <alexander.rivail@gmail.com>
2022-06-08 18:41:06 +0100

    fix

--- src/data/experiences.js
+++ src/data/experiences.js
@@ -15,7 +15,7 @@ export const experiences = {
     {
       title: 'Fullstack Developer',
       company: 'Total Control',
-      description: ' I worked as fulltime with JavaScript, PHP and AngularJs during 12 months to build an internal reseller system.',
+      description: ' I\'ve worked as fulltime with JavaScript, PHP and AngularJs during 12 months to build an internal reseller system.',
       companyLink: 'https://totalcontrol.com.br/novo/',
       companyShortDetail: '',
       date: '2014 - 2015'
@@ -23,63 +23,63 @@ export const experiences = {
     {
       title: 'Mobile Developer',
       company: '7Giros Eventos',
-      description: 'It was a part time position, that I worked building a Mobile App (Ionic) and the backend with Nodejs.',
+      description: 'It was a part time position, that I\'ve worked building a Mobile App (Ionic) and the backend with Nodejs.',
       companyLink: '',
       date: '2015 - 2016'
     },
     {
       title: 'Frontend Developer',
       company: 'E-can',
-      description: 'I worked as fulltime e-commerce developer using Angular and Reactjs.',
+      description: 'I\'ve worked as fulltime e-commerce developer using Angular and Reactjs.',
       companyLink: 'https://www.agenciaecan.com.br/',
       date: '2016 - 2017'
     },
     {
       title: 'Frontend Developer',
       company: 'Way',
-      description: 'I worked as freelancer building a vehicle tracking system using Nodejs and AngularJs.',
+      description: 'I\'ve worked as freelancer building a vehicle tracking system using Nodejs and AngularJs.',
       companyLink: 'https://www.smartway.com.br/',
       date: '2017 - 2018'
     },
     {
       title: 'Fullstack Developer',
       company: '4propety',
-      description: 'I worked as intern freelancer when I was studying english in Ireland. I built some pages with PHP/Wordpress.',
+      description: 'I\'ve worked as intern freelancer when I was studying english in Ireland. I\'ve built some pages with PHP/Wordpress.',
       companyLink: 'https://www.4property.com/',
       date: '2018 - 2018'
     },
     {
       title: 'Frontend Developer',
       company: 'Profit-e E-commerce Solutions',
-      description: 'I worked as fulltime e-commerce developer using Nodejs, Angular and Reactjs.',
+      description: 'I\'ve worked as fulltime e-commerce developer using Nodejs, Angular and Reactjs.',
       companyLink: 'https://www.profite.cx/',
       date: '2018 - 2019'
     },
     {
       title: 'Mobile Developer',
       company: 'Just Fetch it',
-      description: 'I worked as freelancer with Nodejs and Ionic to built a Mobile App.',
+      description: 'I\'ve worked as freelancer with Nodejs and Ionic to built a Mobile App.',
       companyLink: 'https://www.justfetchit.ie/',
       date: '2018 - 2018'
     },
     {
       title: 'Full Stack Developer',
       company: 'Rede Globo',
-      description: 'I worked full time with Nodejs, React Native, ReactJs, VueJs and C#. I built some intern systems and mobile apps.',
+      description: 'I\'ve worked full time with Nodejs, React Native, ReactJs, VueJs and C#. I built some intern systems and mobile apps.',
       companyLink: 'https://redeglobo.globo.com/',
       date: '2019 - 2020'
     },
     {
       title: 'Full Stack Developer',
       company: 'Hammerstone Markets Inc',
-      description: 'I built a messenger platform using Elixir with Phoenix and Vuejs. It was a realtime application (web socket). It was a freelancer.',
+      description: 'I\'ve built a messenger platform using Elixir with Phoenix and Vuejs. It was a realtime application (web socket). It was a freelancer.',
       companyLink: 'https://www.hammerstonemarkets.com/',
       date: '2019 - 2020'
     },
     {
       title: 'Full Stack Developer Frelancer',
       company: 'Opassity',
-      description: 'I worked as freelancer with Nodejs, Mongodb and Vuejs.',
+      description: 'I\'ve worked as freelancer with Nodejs, Mongodb and Vuejs.',
       companyLink: 'https://opassity.netlify.app/',
       date: '2020 - 2022'
     },
