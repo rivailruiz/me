e85e30a672a5b520366bc8bc52613adb13fc0a23
Ruiz <alexander.rivail@gmail.com>
2023-05-18 14:56:41 -0300

    back the dates

--- src/data/experiences.js
+++ src/data/experiences.js
@@ -10,7 +10,7 @@ export const experiences = {
       companyLink: 'https://www.marinha.mil.br/',
       companyShortDetail: '',
       data: '',
-      // date: '2014 - 2015'
+      date: '2014 - 2015'
     },
     {
       title: 'Fullstack Developer',
@@ -18,84 +18,84 @@ export const experiences = {
       description: 'As a fullstack developer, I dedicated 12 months to working with JavaScript, PHP, and AngularJS. My primary responsibility was to build an internal reseller system.',
       companyLink: 'https://totalcontrol.com.br/novo/',
       companyShortDetail: '',
-      // date: '2014 - 2015'
+      date: '2014 - 2015'
     },
     {
       title: 'Mobile Developer',
       company: '7Giros Eventos',
       description: 'During my part-time position, I developed a Mobile App (Ionic) and its backend using Node.js.',
       companyLink: '',
-      // date: '2015 - 2016'
+      date: '2015 - 2016'
     },
     {
       title: 'Frontend Developer',
       company: 'E-can',
       description: 'I served as a full-time e-commerce developer, utilizing Angular and React.js for website development.',
       companyLink: 'https://www.agenciaecan.com.br/',
-      // date: '2016 - 2017'
+      date: '2016 - 2017'
     },
     {
       title: 'Frontend Developer',
       company: 'Way',
       description: 'As a freelancer, I successfully built a vehicle tracking system using Node.js and AngularJS.',
       companyLink: 'https://www.smartway.com.br/',
-      // date: '2017 - 2018'
+      date: '2017 - 2018'
     },
     {
       title: 'Fullstack Developer',
       company: '4property',
       description: 'During my internship in Ireland while studying English, I developed several pages using PHP and WordPress.',
       companyLink: 'https://www.4property.com/',
-      // date: '2018 - 2018'
+      date: '2018 - 2018'
     },
     {
       title: 'Frontend Developer',
       company: 'Profit-e E-commerce Solutions',
       description: 'I worked full-time as an e-commerce developer, utilizing Node.js, Angular, and React.js for website development.',
       companyLink: 'https://www.profite.cx/',
-      // date: '2018 - 2019'
+      date: '2018 - 2019'
     },
     {
       title: 'Mobile Developer',
       company: 'Just Fetch it',
       description: 'As a freelancer, I utilized Node.js and Ionic to successfully build a Mobile App.',
       companyLink: 'https://www.justfetchit.ie/',
-      // date: '2018 - 2018'
+      date: '2018 - 2018'
     },
     {
       title: 'Full Stack Developer',
       company: 'Rede Globo',
       description: 'I worked full-time, utilizing Node.js, React Native, React.js, Vue.js, and C#. I developed several internal systems and mobile apps.',
       companyLink: 'https://redeglobo.globo.com/',
-      // date: '2019 - 2020'
+      date: '2019 - 2020'
     },
     {
       title: 'Full Stack Developer',
       company: 'Hammerstone Markets Inc',
       description: 'I developed a real-time messenger platform as a freelancer using Elixir with Phoenix and Vue.js. The application incorporated web socket technology.',
       companyLink: 'https://www.hammerstonemarkets.com/',
-      // date: '2019 - 2020'
+      date: '2019 - 2020'
     },
     {
       title: 'Full Stack Developer Freelancer',
       company: 'Opassity',
       description: 'As a freelancer, I specialized in Node.js, MongoDB, and Vue.js (Web) and React Native (mobile). I also managed the infrastructure using AWS services such as Lambda, S3, EC2, CloudFront, and RDS.',
       companyLink: 'https://opassity.netlify.app/',
-      // date: '2020 - 2022'
+      date: '2020 - 2022'
     },
     {
       title: 'Full Stack Developer',
       company: 'Picoworkers',
       description: 'In my role as a Full Stack Developer, I primarily worked with PHP and MySQL on the backend. I actively contributed to the development of new features and maintenance of existing ones.',
       companyLink: 'https://www.picoworkers.com/',
-      // date: '2019 - 2022'
+      date: '2019 - 2022'
     },
     {
       title: 'Full Stack Developer',
       company: 'Grupo Soma',
       description: ' I worked as a Full Stack Developer at Grupo Soma, where I specialized in utilizing Node.js, Nest.js, Redis, Terraform, Google Cloud, and MySQL. During my tenure, I primarily focused on successfully migrating a monolithic application to a microservices architecture resulting in improved scalability, performance and leading to a significant reduction in infrastructure costs. However, I also had the opportunity to contribute to the frontend development using React, specifically for an ecommerce platform. While my main expertise lies in the backend, I successfully utilized React to build and implement various features, enhancing the platform\'s user interface and overall functionality. ',
       companyLink: 'https://www.somagrupo.com.br',
-      // date: '2022 - 2023'
+      date: '2022 - 2023'
     }
   ]
 };
