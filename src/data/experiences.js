7e7174419ff66d4f2d3c99fa30426b600b807a0a
Alexander Ruiz <ruizalexander@id.uff.br>
2024-12-30 20:45:22 +0000

    update roles

--- src/data/experiences.js
+++ src/data/experiences.js
@@ -96,6 +96,20 @@ export const experiences = {
       description: ' I worked as a Full Stack Developer at Grupo Soma, where I specialized in utilizing Node.js, Nest.js, Redis, Terraform, Google Cloud, and MySQL. During my tenure, I primarily focused on successfully migrating a monolithic application to a microservices architecture resulting in improved scalability, performance and leading to a significant reduction in infrastructure costs. However, I also had the opportunity to contribute to the frontend development using React, specifically for an ecommerce platform. While my main expertise lies in the backend, I successfully utilized React to build and implement various features, enhancing the platform\'s user interface and overall functionality. ',
       companyLink: 'https://www.somagrupo.com.br',
       date: '2022 - 2023'
+    },
+    {
+      title: 'Full Stack Developer',
+      company: 'Prefeitura do Rio de Janeiro',
+      description: "In my role as a Full Stack Engineer at the Prefeitura do Rio de Janeiro, I\'m deeply involved in crafting and refining advanced tech solutions. My day-to-day revolves around a diverse set of responsibilities, all aimed at ensuring our software meets the highest standards. At the heart of my work lies the backend development using Node.js and the NestJS framework. It\'s here that I lay the foundation for our applications, ensuring they\'re not just functional but also scalable and reliable. Working hand-in-hand with our DevOps team, I dive into Kubernetes and Google Cloud Platform, ensuring our deployments are seamless and efficient. On the frontend, I'm passionate about crafting intuitive user experiences with React. Whether it's utilizing Redux for state management or diving into React Hooks for added functionality, I'm always looking for ways to elevate our interfaces. Database management is another key aspect of my role, with PostgreSQL being our tool of choice. Ensuring data integrity and performance is crucial, and I take great care in maintaining our databases. But perhaps the most rewarding part of my job is mentoring our junior developers. Being able to share my knowledge and watch them grow not only as developers but as individuals is truly fulfilling.",
+      companyLink: 'https://prefeitura.rio',
+      date: '2023 - 2024'
+    },
+    {
+      title: 'CTO',
+      company: 'FashionAI',
+      description: "As the Founder and CTO of FashionAI, I lead all technology-related aspects of the company, driving innovation and efficiency across our platforms. My responsibilities include overseeing generative AI development, managing DevOps practices, building CI/CD pipelines, and ensuring robust infrastructure. I actively contribute to coding, utilizing a microservices architecture with services in Node.js and Python. Our stack includes Kubernetes deployments on both GKE (Google Cloud Platform) and EKS (AWS), alongside databases such as PostgreSQL, MongoDB and Qdrant. This role combines hands-on technical leadership with strategic vision to support our mission of delivering cutting-edge AI-driven solutions.",
+      companyLink: 'https://generativecrm.com',
+      date: '2024 - present'
     }
   ]
 };
