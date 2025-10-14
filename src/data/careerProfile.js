e62cf582711ef57ebef0ff07da2e75e4e5ec4a86
Alexander Ruiz <ruizalexander@id.uff.br>
2025-10-14 13:50:44 -0300

    updates

--- src/data/careerProfile.js
+++ src/data/careerProfile.js
@@ -1,7 +1,7 @@
 export const careerProfile = {
   sectionTitle: 'Career Profile',
   icon: null,
-  description: '<p> I am a highly experienced software developer with almost 10 years of expertise in development. Throughout my career, I have been involved in various challenging and creative projects, constantly striving to improve my programming skills and explore new concepts. I have a strong passion for collaboration and enjoy working with teams to achieve innovative solutions. In addition to my professional work, I also find fulfillment in sharing my knowledge and teaching my friends about the things I have learned along the way.</p>'
+  description: '<p> I am a highly experienced software developer with 10+ years of expertise in development. Throughout my career, I have been involved in various challenging and creative projects, constantly striving to improve my programming skills and explore new concepts. I have a strong passion for collaboration and enjoy working with teams to achieve innovative solutions. In addition to my professional work, I also find fulfillment in sharing my knowledge and teaching my friends about the things I have learned along the way. <br><br> My focus now is on generative AI, where I specialize in model fine-tuning, prompt engineering, and creating custom solutions tailored to unique business needs. This work includes extensive experimentation with large language models (LLMs) and machine learning workflows. I also bring DevOps experience, with skills in AWS, Google Cloud, and automation tools like Terraform and Kubernetes, enabling efficient deployment and scalability of AI solutions.</p>'
 };
 
 export default careerProfile;
