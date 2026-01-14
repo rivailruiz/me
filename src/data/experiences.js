eb58f0b31f4b652528dad1d0be7fdebfb38fbcf0
Alexander Ruiz <ruizalexander@id.uff.br>
2026-01-14 16:45:19 -0300

    update cv to IA positions

--- src/data/experiences.js
+++ src/data/experiences.js
@@ -4,65 +4,6 @@ export const experiences = {
   description: null,
   list: [
     {
-      title: 'Backend Intern',
-      company: 'Brazil\'s Navy',
-      description: 'I worked as a backend intern for 8 months, utilizing PHP to contribute to the development of a weather system API.',
-      companyLink: 'https://www.marinha.mil.br/',
-      companyShortDetail: '',
-      data: '',
-      date: '2014 - 2015'
-    },
-    {
-      title: 'Fullstack Developer',
-      company: 'Total Control',
-      description: 'As a fullstack developer, I dedicated 12 months to working with JavaScript, PHP, and AngularJS. My primary responsibility was to build an internal reseller system.',
-      companyLink: 'https://totalcontrol.com.br/novo/',
-      companyShortDetail: '',
-      date: '2014 - 2015'
-    },
-    {
-      title: 'Mobile Developer',
-      company: '7Giros Eventos',
-      description: 'During my part-time position, I developed a Mobile App (Ionic) and its backend using Node.js.',
-      companyLink: '',
-      date: '2015 - 2016'
-    },
-    {
-      title: 'Frontend Developer',
-      company: 'E-can',
-      description: 'I served as a full-time e-commerce developer, utilizing Angular and React.js for website development.',
-      companyLink: 'https://www.agenciaecan.com.br/',
-      date: '2016 - 2017'
-    },
-    {
-      title: 'Frontend Developer',
-      company: 'Way',
-      description: 'As a freelancer, I successfully built a vehicle tracking system using Node.js and AngularJS.',
-      companyLink: 'https://www.smartway.com.br/',
-      date: '2017 - 2018'
-    },
-    {
-      title: 'Fullstack Developer',
-      company: '4property',
-      description: 'During my internship in Ireland while studying English, I developed several pages using PHP and WordPress.',
-      companyLink: 'https://www.4property.com/',
-      date: '2018 - 2018'
-    },
-    {
-      title: 'Frontend Developer',
-      company: 'Profit-e E-commerce Solutions',
-      description: 'I worked full-time as an e-commerce developer, utilizing Node.js, Angular, and React.js for website development.',
-      companyLink: 'https://www.profite.cx/',
-      date: '2018 - 2019'
-    },
-    {
-      title: 'Mobile Developer',
-      company: 'Just Fetch it',
-      description: 'As a freelancer, I utilized Node.js and Ionic to successfully build a Mobile App.',
-      companyLink: 'https://www.justfetchit.ie/',
-      date: '2018 - 2018'
-    },
-    {
       title: 'Full Stack Developer',
       company: 'Rede Globo',
       description: 'I worked full-time, utilizing Node.js, React Native, React.js, Vue.js, and C#. I developed several internal systems and mobile apps.',
